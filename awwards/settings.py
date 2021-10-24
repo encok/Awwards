808f974ca501140e62fa216d36a5c4a0972fa848
enock <enckkipronoh@gmail.com>
2021-10-24 09:22:57 +0300

    configuring the database

--- awwards/settings.py
+++ awwards/settings.py
@@ -80,11 +80,16 @@ WSGI_APPLICATION = 'awwards.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+     'default': {
+         'ENGINE': 'django.db.backends.postgresql',
+         'NAME': 'awwards',
+         'USER': 'postgres',
+     'PASSWORD':'enock',
+     # 'host':'127.0.0.1',
+     #  'PORT':'5432',
+     }
 }
+
 
 
 # Password validation
